ba47b284773857775f280cb8c0585780ab585bb0
ofabiocarvalho <ofabiocarvalho@gmail.com>
2020-04-06 16:38:25 -0300

    criação do projeto

---
+++ src/ifood-api/src/main/java/com/fiap/ifood/IfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/ifood-api/src/test/java/com/fiap/ifood/IfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
